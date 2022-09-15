18bb48f4a2d931220d7093348fc6bc0091141dcc
ThomLb <thomaslobello01@gmail.com>
2022-09-15 17:45:35 -0400

    Add files via upload
    Made my school and lib

---
+++ library.py
@@ -0,0 +1,16 @@
+from tkinter import Image
+
+
+def draw_library():
+    print("here is you lib")
+    lib = """
+        --------------
+        |            |        
+        |            |
+        |     LIB    |
+        |            |
+        --------------
+    """
+    print(lib)
+    return
+draw_library()
---
+++ school.py
@@ -0,0 +1,16 @@
+
+
+def draw_school():
+    print("school right here")
+    school = """
+        --------------
+        |            |        
+        |            |
+        |   school   |
+        |            |
+        --------------
+    """
+    print(school)
+    return
+
+draw_school()
