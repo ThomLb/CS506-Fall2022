9e86388c431b779be3d9c8ec0062f585ee3e8b8f
Thomas Lo Bello <thomaslb@bu.edu>
2022-09-22 17:55:56 -0400

    ready

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -3,4 +3,10 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    matrix = [[]]
+    f = open(csv_file_path, 'r')
+    f.readline().rstrip()
+    for row in matrix:
+        for col in matrix:
+            matrix[row][col] = 
+            
